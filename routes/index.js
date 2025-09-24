022900a5c98029b412412a6ae3e32b3e73ff3e74
Kajal Saini <sizukabts91@gmail.com>
2025-09-24 20:11:41 +0530

    create users.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const localStrategy=require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
+
+router.get('/', function (req, res) {
+  res.render("index");
 });
 
+router.post("/register", function (req, res) {
+  const {username,email,fullname}=req.body;
+  const userData=new userModel({username,email,fullname});
+
+  userModel.register(userData, req.body.password) 
+  .then(function(){
+    passport.authenticate("local")(req,res.function(){
+      res.redirect("/profile")
+    })
+  })
+  })
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm=require("passport-local-mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/nayaapp");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+  }],
+  dp: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  fullname: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = router;
+userSchema.plugin(plm);
+module.exports = mongoose.model("User", userSchema);
